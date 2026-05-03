bc1878c51226a223fe69c16066eca6f4730ec321
Stefan Bobrowski <stefanbobrowski1@gmail.com>
2026-05-03 17:25:04 -0500

    add logs folder if none

--- backend/src/services/logger.ts
+++ backend/src/services/logger.ts
@@ -20,5 +20,6 @@ export const logEvent = (event: string, data: any, user?: LogUser) => {
     ? JSON.parse(fs.readFileSync(logFile, 'utf-8'))
     : [];
   logs.push(logEntry);
+  fs.mkdirSync(path.dirname(logFile), { recursive: true });
   fs.writeFileSync(logFile, JSON.stringify(logs, null, 2));
 };
